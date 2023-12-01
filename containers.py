0706f5e99bb082c2e67c144e6480a88162939010
caitlincorrigan <caitlinfcorrigan@gmail.com>
2023-12-01 14:09:35 -0800

    completes exercise 1

---
+++ containers.py
@@ -0,0 +1,14 @@
+# Exercise 1
+students = ["Ada", "Bev", "Cathy"]
+print(students[1])
+print(students[-1])
+
+# Exercise 2
+
+
+# Exercise 3
+# Exercise 4
+# Exercise 5
+# Exercise 6
+# Exercise 7
+# Exercise 8
